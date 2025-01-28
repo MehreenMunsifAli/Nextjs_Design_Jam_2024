5394f4947621d98926c7cebc196aa70f1082e8f0
MehreenMunsifAli <mehreen.munsif.ali@gmail.com>
2025-01-28 19:55:39 +0500

    Update UI

--- src/components/FoodCategory.tsx
+++ src/components/FoodCategory.tsx
@@ -18,45 +18,45 @@ export default function FoodCategory() {
             {/* Images Content */}
             <div className="container flex flex-col lg:flex-row justify-between items-center flex-wrap lg:flex-nowrap my-16 gap-4 ">
                 {/* Single image with save 30% div */}
-                <div className="relative w-full h-[700px] md:w-[244px] md:h-[294px] hover:scale-105 transition duration-300">
+                <div className="relative w-[264px] md:w-[244px] h-[294px] hover:scale-105 transition duration-300">
                     <Image
-                        className="object-cover rounded-md"
+                        className="object-contain rounded-md"
                         src={"/assets/images/food_category/unsplash_-lHZUkiWM74.svg"}
                         alt="Eggs Image"
                         fill
                     />
                     <div className="absolute inset-y-1/3 md:inset-y-24 text-center flex flex-col items-end gap-4">
-                        <div className="px-12 py-5 md:px-4 md:py-2 bg-white text-[#FF9F0D] flex items-center justify-center rounded-lg">
-                            <p className="text-5xl md:text-[18px] font-bold">Save 30%</p>
+                        <div className="px-8 py-3 md:px-4 md:py-2 bg-white text-[#FF9F0D] flex items-center justify-center rounded-lg">
+                            <p className="text-2xl md:text-[18px] font-bold">Save 30%</p>
                         </div>
-                        <div className="px-12 py-5 md:px-4 md:py-2 text-white bg-[#FF9F0D] flex items-center justify-center rounded-lg">
-                            <p className="text-5xl md:text-[20px] font-bold">Fast Food Dish</p>
+                        <div className="px-8 py-3 md:px-4 md:py-2 text-white bg-[#FF9F0D] flex items-center justify-center rounded-lg">
+                            <p className="text-2xl md:text-[20px] font-bold">Fast Food Dish</p>
                         </div>
                     </div>
 
                 </div>
                 
-                <div className=" relative w-full h-[700px] md:w-[244px] md:h-[294px] hover:scale-105 transition duration-300">
+                <div className=" relative w-[264px] md:w-[244px] h-[294px] hover:scale-105 transition duration-300">
                     <Image
-                        className="object-cover rounded-md"
+                        className="object-contain rounded-md"
                         src={"/assets/images/food_category/unsplash_dphM2U1xq0U.svg"}
                         alt="Main Course Image"
                         fill
                     />
                 </div>
 
-                <div className=" relative w-full h-[700px] md:w-[244px] md:h-[294px] hover:scale-105 transition duration-300">
+                <div className=" relative w-[264px] md:w-[244px] h-[294px] hover:scale-105 transition duration-300">
                     <Image
-                        className="object-cover rounded-md"
+                        className="object-contain rounded-md"
                         src={"/assets/images/food_category/unsplash_MRHyv-hHxgk.svg"}
                         alt="Main Course Image"
                         fill
                     />
                 </div>
                 
-                <div className=" relative w-full h-[700px] md:w-[244px] md:h-[294px] hover:scale-105 transition duration-300">
+                <div className=" relative w-[264px] md:w-[244px] h-[294px] hover:scale-105 transition duration-300">
                     <Image
-                        className="object-cover rounded-md"
+                        className="object-contain rounded-md"
                         src={"/assets/images/food_category/unsplash_q54Oxq44MZs.svg"}
                         alt="Sandwich"
                         fill
